c33cf9618c26fae198237f0453bdd7e74b4b814c
Rajat Pathak <rajatpathak786@gmail.com>
2019-07-23 20:12:37 +0530

    page transitions for node w.i.p

--- transition.js
+++ transition.js
@@ -1,18 +1,29 @@
-// $(function(){
-//   $('#nodel1').on('click', function(){
-//     console.log(this);
-//     console.log('asasdasdasdasdasd');
-//   });
-// })
+// import Swup from 'swup';
 
+// const swup = new Swup();
+$('.page.home').show();
 $(document).ready(function(){
   $('svg').click(function() {
     $('#node2').on('click', function(e){
       if (e.handled != true) {
-        console.count('nodel1_____________________________!!!!!!!!!!!!!!!!!!!!!!!!!!!')
-        alert('asdasdasdasdsd');
+        $('.page').hide();
+        $('.node2').show();
+        e.handled = true;
+      }
+    })
+    $('#node1').on('click', function(e){
+      if (e.handled != true) {
+        $('.page').hide();
+        $('.node1').show();
         e.handled = true;
       }
     })
   })
 });
+$('.index').click(function(e) {
+  if (e.handled != true) {
+    $('.page').hide();
+    $('.page.home').show();
+    e.handled = true;
+  }
+})
