a4275021f2b00d41372940161b54ea5a49a976e4
Rajat Pathak <rajatpathak786@gmail.com>
2019-07-26 18:45:32 +0530

    added transitions on svg node first node hide component and displays transition on other section and second node displays transitions on same home section

--- transition.js
+++ transition.js
@@ -1,46 +1,61 @@
-let node_transition, r = 18, displayout;
+let node_transition, displayout, r;
 var svgNS = "http://www.w3.org/2000/svg";
 $('.page.home').show();
 $(document).ready(function(){
-  $('svg').click(function() {
-    $('#node1').on('click', function(e){
-      if (e.handled != true) {
-        $('.page').hide();
-        $('.node1').show();
-        animateNode();
-        e.handled = true;
-      }
-    })
-    $('#node2').on('click', function(e){
-      if (e.handled != true) {
-        $('.page').hide();
-        $('.node2').show();
-        e.handled = true;
-      }
-    })
-    $('#node3').on('click', function(e){;
-      if (e.handled != true) {
-        $('.page').hide();
-        $('.node3').show();
-        e.handled = true;
-      }
-    })
+  $('svg').on('click', '#node1',function(e) {
+    // $('#node1').on('click', function(e){
+    if (e.handled != true) {
+      $('.page').hide();
+      $('.node1').show();
+      animateNode('65.2%', '9.2%', 'node1');
+      e.handled = true;
+    }
   })
-});
-$('.index').click(function(e) {
-  if (e.handled != true) {
-    // node_transition = false;
-    // r = 18;
-    // document.getElementById('node_transition').remove();
-    // $('.page').hide();
-    // $('.page.home').show();
-    displayout = true;
-    animateNode();
-    e.handled = true;
-  }
+  $('svg').on('click', '#node2',function(e) {
+    if (e.handled != true) {
+      animateNode('37.5%', '13.338%', 'node2');
+      e.handled = true;
+    }
+  })    
+    // })
+    // $('#node2').on('click', function(e){
+    //   if (e.handled != true) {
+    //     animateNode('37.5%', '13.338%', 'node2');
+    //     e.handled = true;
+    //   }
+    // })
+    // $('#node3').on('click', function(e){;
+    //   if (e.handled != true) {
+    //     $('.page').hide();
+    //     $('.node3').show();
+    //     e.handled = true;
+    //   }
+    // })
+    // $('#node_transition').on('click', function(e) {
+    //   if (e.handled != true) {
+    //     displayout = true;
+    //     animateNode();
+    //     e.handled = true;
+    //   }
+    // })
 })
+// });
 
-function animateNode() {
+// $('.index').click(function(e) {
+//   if (e.handled != true) {
+//     // node_transition = false;
+//     // r = 18;
+//     // document.getElementById('node_transition').remove();
+//     // $('.page').hide();
+//     // $('.page.home').show();
+//     displayout = true;
+//     animateNode();
+//     e.handled = true;
+//   }
+// })
+
+function animateNode(x, y, id) {
+  console.log(x, y, id);
   if(document.getElementById('section1').style.display == 'block') {
     // let a = 0
     // for (let r = 20; r <= 200; r = r + 2) {
@@ -57,45 +72,61 @@ function animateNode() {
     //   await display(r); 
     // }
     if (displayout) {
-      displayOut();  
+      displayOut(x, y, id);  
     } else {
-      display();
+      r = 20;
+      console.log(x, y, id);
+      display(x, y, id);
     }
-    
     // CreateCircle('66%', '10%', '20', 'red', 'node_transition');
     // document.getElementById('node_transition').remove();
   }
+  else if (document.getElementById('home').style.display == 'block') {
+    if (displayout) {
+      displayOut(x, y, id);  
+    } else {
+      r = 20;
+      console.log(x, y, id, r);
+      display(x, y, id);
+    }
+  }
 }
 
-function display() {
+function display(x, y, id) {
   r = r + 2;
+  console.log(x, y, id);
   // if (node_transition) {
   //   node_transition = false;
   //   document.getElementById('node_transition').remove();
   // }
   if (r <= 200) {
     setTimeout(() => {
-      CreateCircle('65.2%', '9.2%', r, 'red', 'node_transition');
+      // CreateCircle('65.2%', '9.2%', r, 'red', 'node_transition');
+      node_transition = true;
+      CreateCircle(x, y, r, 'red', id);
     }, 10);
   }
 }
 
-function displayOut() {
+function displayOut(x, y, id) {
   console.log(r, "displayOut");
   r = r - 2;
   if (r >= 20) {
     setTimeout(() => {
-      CreateCircle('65.2%', '9.2%', r, 'red', 'node_transition');
+      // CreateCircle('65.2%', '9.2%', r, 'red', 'node_transition');
+      node_transition = true;
+      CreateCircle(x, y, r, 'red', id);
     }, 10);
   }
   
 }
 
 function CreateCircle(x, y, r, color, id) {
-  console.log(r, "Create Circle");
+  console.log(x, y, id);
+  console.log(r, node_transition, "Create Circle");
   if (node_transition) {
     node_transition = false;
-    document.getElementById('node_transition').remove();
+    document.getElementById(id).remove();
   }
   var myCircle = document.createElementNS(svgNS, "circle"); //to create a circle. for rectangle use "rectangle"
   myCircle.setAttributeNS(null, "id", id);
@@ -104,24 +135,36 @@ function CreateCircle(x, y, r, color, id) {
   myCircle.setAttributeNS(null, "r", r);
   myCircle.setAttributeNS(null, "fill", color);
   myCircle.setAttributeNS(null, "stroke", "black");
-  document.getElementById("mySVG_node1").appendChild(myCircle);
-  node_transition = true;
-
+  if(document.getElementById('section1').style.display == 'block') {
+    document.getElementById("mySVG_node1").appendChild(myCircle);
+  } else {
+    document.getElementById("mySVG").appendChild(myCircle);
+  }
   if (displayout) {
     console.log('displayout enter', r);
     if (r != 20) {
       console.log('displayout update condition', r);
-      displayOut();
+      displayOut(x, y, id);
     } else {
       console.log('displayout final', r);
-      node_transition = false;
-      r = 18;
-      document.getElementById('node_transition').remove();
+      r = 20;
       displayout = false;
-      $('.page').hide();
-      $('.page.home').show();
+      if (document.getElementById('home').style.display == 'block') {
+        node_transition = true;
+        CreateCircle(x, y, r, 'red', id);
+        displayout = false;         
+      } else {
+        $('.page').hide();
+        $('.page.home').show();
+        node_transition = true;
+        CreateCircle(x, y, r, 'red', id);
+        displayout = false;
+      }
     }
-  } else if (r != 200) {
-      display();
+  } else if (r != 200 && r !=20) {
+      display(x, y, id);
+  } else {
+    displayout = true;
+    console.log(displayout, node_transition, r)
   }
 }
